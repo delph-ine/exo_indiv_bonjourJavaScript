39d2c70595627bc69eb50c8c5be67cc9ef820a40
delph-ine <o.df@free.fr>
2024-10-17 15:43:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+//let message = "bonjour"
+//console.log(message);
+
+
+//etape 2 premier code
+
+//let message = bonjour !
+//console.log(message);
+//declration de la variable firstname 
+//j'ai initialiser ma valeur
+let firstname = "blublu"
+//j'ai assignée 2 nouvelles valeurs
+firstname = "blabla"
+firstname = "florence"
+//appelle de la variable firstname
+console.log(firstname)
+//declare ma variable firstname dans message
+let message ="bonjour" + " " + firstname + " ! "
+console.log(message)
+//firstname =  "bonjour !" + "blublu" = firstname;
+
+
+//étape 3 dans une fonction
+//let message ="bonjour !"
+//console.log(message)
+
+let firstname = "blublu"
+//appelle de la variable firstname
+console.log(firstname)
+//declare ma variable firstname dans message
+let message ="bonjour" + " " + firstname + " ! "
+console.log(message)
+
+//declration de message firstame
+//let messagefirstname = "bonjour blublu !"
+//appelle de la variable 
+//console.log(messagefirstname)
+//declaration fonction sayhello
+//let sayhello = (messagefirstname)
+//appelle de la fonction sayhello()
+//console.log(sayhello)
+
+//je declare ma fonction seyhello pour que firstname devienne un parametre
+//function seyhello()
+//{
+//console.log(firstname)
+//}
+
+
+
+
